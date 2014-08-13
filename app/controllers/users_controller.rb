fe022af83455b1c833648506fb114d2be8f7fd17
carolynthu <ctnguyen.cn@gmail.com>
2014-08-13 14:03:12 -0700

    fixed edit button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @boards = current_user.boards
+    @boards = current_user.boards.all
     @board = current_user.boards.new
   end
 
